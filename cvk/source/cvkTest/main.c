636f3d53c5a56023956230a7cf21267576f86e5b
dbuckstein <dbuckstein@users.noreply.github.com>
2020-12-29 18:00:48 -0500

    Setting up for initial development. MOD: ignore. ADD: Visual Studio solution and project with dummy starter source.

---
+++ cvk/source/cvkTest/main.c
@@ -0,0 +1,49 @@
+/*
+   Copyright 2020 Daniel S. Buckstein
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
+*/
+
+/*
+	cvk: Vulkan renderer from scratch in C.
+	By Daniel S. Buckstein
+
+	main.c
+	Entry point source for application.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "cvk/cvk.h"
+
+
+//-----------------------------------------------------------------------------
+
+#ifdef _WIN32
+// Windows entry point.
+int __stdcall WinMain(
+	void const* const hInstance,
+	void const* const hPrevInstance,
+	char const* const lpCmdLine,
+	int const nCmdShow)
+#else	// !_WIN32
+
+#endif	// _WIN32
+{
+
+}
+
+
+//-----------------------------------------------------------------------------
